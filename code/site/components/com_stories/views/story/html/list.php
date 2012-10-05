319791cabbc6b1ee29596bef7319e1f272457d55
asanieyan <asanieyan@a933e228-e664-4d3f-8241-49479697c698>
2012-10-05 01:33:12 +0000

    Fix: since stories are no longer commentable then we always need to pass the story->objects to render comments
    git-svn-id: file:///var/www/svn/anahita/anahita/branches/search@15885 a933e228-e664-4d3f-8241-49479697c698

--- code/site/components/com_stories/views/story/html/list.php
+++ code/site/components/com_stories/views/story/html/list.php
@@ -14,13 +14,17 @@
         <div class="story-timestamp">
             <?= @date($timestamp) ?>
         </div>
+        <?php
+           //if the comment command exists that means
+           //we can comment. If we can comment then
+           //that means story has a commentable object
+        ?>
         <?php $can_comment = $commands->offsetExists('comment') ?>
         <?= @helper('ui.commands', $commands)?>
         
-        
         <div id="<?= 'story-comments-'.$item->id?>" class="story-comments">
             <?php if ( !empty($comments) || $can_comment ) : ?>
-            <?= @helper('ui.comments', $story, array('comments'=>$comments, 'can_comment'=>$can_comment, 'pagination'=>false, 'show_guest_prompt'=>false, 'truncate_body'=>array('consider_html'=>true, 'read_more'=>true))) ?>
+            <?= @helper('ui.comments', $story->object, array('comments'=>$comments, 'can_comment'=>$can_comment, 'pagination'=>false, 'show_guest_prompt'=>false, 'truncate_body'=>array('consider_html'=>true, 'read_more'=>true))) ?>
             <?php endif;?>
             <?php if( !empty($comments) && $can_comment ): ?>
             <div class="comment-overtext-box">  
