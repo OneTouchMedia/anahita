5ee7cb30b11eb0a1f5c158bc01d2d0f71f76aa64
asanieyan <asanieyan@a933e228-e664-4d3f-8241-49479697c698>
2012-10-24 01:33:43 +0000

    [added] ability to validate password [added] creating new person
    git-svn-id: file:///var/www/svn/anahita/anahita/branches/search@16020 a933e228-e664-4d3f-8241-49479697c698

--- code/site/components/com_people/controllers/person.php
+++ code/site/components/com_people/controllers/person.php
@@ -79,11 +79,89 @@ class ComPeopleControllerPerson extends ComActorsControllerDefault
     {
         parent::_actionDelete($context);
         
-        $this->commit($context);
+        $this->commit();
         
         JFactory::getUser($this->getItem()->userId)->delete();     
         
         $this->getService('com:people.helper.person')->logout($this->getItem(), array('message'=>JText::_('COM-PEOPLE-PERSON-DELETED-MESSAGE')));               
+    }
+    
+    /**
+     * Returns whether registration is possible or not. The user can not 
+     * registered if it's already logged in or the configuration value
+     * 'users.allowUserRegistration' is set to no
+     * 
+     * @return boolean Returns whether registration is possible or not.
+     */
+    public function canAdd()
+    {
+        if ( JFactory::getUser()->id > 0 )
+            return false;
+            
+//        if ( !get_config_value('users.allowUserRegistration', true) )
+//            return false;        
+    }
+    
+    /**
+     * Person add action creates a new person object.
+     * 
+     * @param KCommandContext $context Commaind chain context
+     * 
+     * @return AnDomainEntityAbstract
+     */
+    protected function _actionAdd(KCommandContext $context)
+    {
+        //we are not saving this person but just validating it
+        $person = parent::_actionAdd($context);
+        $data   = $context->data;        
+        
+        $person->userId = PHP_INT_MAX; //is assiged automatically
+        
+        
+        //manually set the password to make sure there's a password
+        
+        $person->setPassword($data->password);
+        
+        //add the validations here
+        $this->getRepository()->getValidator()
+                ->addValidation('username','uniqueness')
+                ->addValidation('email',   'uniqueness')
+                ;
+                
+        if ( $person->validate() === false ) {
+            $context->setError(new AnErrorSet($person->getErrors(), KHttpResponse::BAD_REQUEST));
+            return false;
+        }
+             
+        $person->reset();
+        
+        $user       = clone JFactory::getUser();
+        $authorize  =& JFactory::getACL();
+        
+        $user->set('id', 0);
+        $user->set('name', $person->name);
+        $user->set('username', $person->username);
+        $user->set('email', $person->email);
+        $user->set('password', $person->getPassword(true));
+        $user->set('usertype', 'Registered');
+        $user->set('gid', $authorize->get_group_id( '', 'Registered', 'ARO' ));
+        $date =& JFactory::getDate();
+        $user->set('registerDate', $date->toMySQL());
+        if ( get_config_value('users.useractivation', false) ) {
+            jimport('joomla.user.helper');
+            $user->set('activation', JUtility::getHash( JUserHelper::genRandomPassword()) );
+            $user->set('block', '1');
+        }
+        $user->save();
+        
+        //set the status
+        $context->status  = KHttpResponse::CREATED;
+        //return a custom tag for user activiation required
+        $context->append(array(
+            'headers' => array(
+              'User-Activation-Required' => get_config_value('users.useractivation')
+        )));
+        
     }
     
     /**
