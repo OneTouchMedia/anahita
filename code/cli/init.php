7a1731356e1400c92aa2bb9aecd58707ddd83ea3
asanieyan <asanieyan@a933e228-e664-4d3f-8241-49479697c698>
2012-10-31 21:02:32 +0000

    init.php loads the framework.php now
    git-svn-id: file:///var/www/svn/anahita/anahita/branches/search@16074 a933e228-e664-4d3f-8241-49479697c698

--- code/cli/init.php
+++ code/cli/init.php
@@ -1,43 +1,23 @@
 <?php
 
-// Set flag that this is a parent file
-define( '_JEXEC', 1 );
-if ( !isCli() )
-{
-    die('Not Allowed');
-}
+PHP_SAPI == 'cli' or die('Access denied');
+
 if ( strpos($_SERVER['SCRIPT_NAME'],'/') === 0 )
     $__FILE__ = $_SERVER['SCRIPT_NAME'];
 else
     $__FILE__ = $_SERVER['PWD'].'/'.$_SERVER['SCRIPT_NAME'];
-$dir      = realpath(preg_replace('/\/cli.*/','', $__FILE__));
+$dir  = realpath(preg_replace('/\/cli.*/','', $__FILE__));
 
 define('JPATH_BASE', $dir);
-define( 'DS', DIRECTORY_SEPARATOR );
-require_once ( JPATH_BASE .DS.'includes'.DS.'defines.php' );
-require_once ( JPATH_BASE .DS.'includes'.DS.'framework.php' );
-jimport('joomla.plugin.helper');
-$mainframe =& JFactory::getApplication('site', array('session'=>false));
-JFactory::getConfig()->setValue('cache_handler','');
-JFactory::getConfig()->setValue('session_handler','');
-JPluginHelper::importPlugin('system', 'anahita');
-JPluginHelper::importPlugin('notification');
-JFactory::getLanguage()->load('lib_anahita');
+
+require_once JPATH_BASE.'/includes/framework.php';
+
+KService::get('com://site/application.dispatcher')->load();
 
 KServiceIdentifier::setApplication('dev', JPATH_BASE.'/cli');
 
-function devtool_exception_handler($e)
-{
+function cli_exception_handler($e) {
     print "\n".'Exception : '.$e->getMessage()."\n\n";   
 }
 
-function isCli() {
-
-    if(php_sapi_name() == 'cli' && empty($_SERVER['REMOTE_ADDR'])) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-set_exception_handler('devtool_exception_handler');
+set_exception_handler('cli_exception_handler');
