0699e28e758e19c108f9da6ca504288ff840cce0
asanieyan <asanieyan@a933e228-e664-4d3f-8241-49479697c698>
2012-11-08 23:52:16 +0000

    added the search functionality
    git-svn-id: file:///var/www/svn/anahita/anahita/branches/search@16095 a933e228-e664-4d3f-8241-49479697c698

---
+++ code/site/components/com_search/controllers/search.php
@@ -0,0 +1,65 @@
+<?php
+
+/** 
+ * LICENSE: ##LICENSE##
+ * 
+ * @category   Anahita
+ * @package    Com_Search
+ * @subpackage Controller
+ * @author     Arash Sanieyan <ash@anahitapolis.com>
+ * @author     Rastin Mehr <rastin@anahitapolis.com>
+ * @copyright  2008 - 2010 rmdStudio Inc./Peerglobe Technology Inc
+ * @license    GNU GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
+ * @version    SVN: $Id: resource.php 11985 2012-01-12 10:53:20Z asanieyan $
+ * @link       http://www.anahitapolis.com
+ */
+
+/**
+ * Search controller searches the node or searchable entities and display the
+ * result.
+ * 
+ * The search controller searches name and body of the nodes that are searchable (specicifed by app delegate)
+ * for the requested keyword. Once the result is returned, it pass the search result
+ * to each app to render
+ * 
+ * 
+ * @category   Anahita
+ * @package    Com_Search
+ * @subpackage Controller
+ * @author     Arash Sanieyan <ash@anahitapolis.com>
+ * @author     Rastin Mehr <rastin@anahitapolis.com>
+ * @license    GNU GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
+ * @link       http://www.anahitapolis.com
+ */
+class ComSearchControllerSearch extends ComBaseControllerResource
+{
+    /**
+     * Search and return the result 
+     * 
+     * @param KCommandContext $context Controller command chain context
+     * 
+     * @return string The result to render
+     */
+    protected function _actionGet(KCommandContext $context)
+    {
+        $repos = $this->getService('repos://site/base.node');
+        $query = $repos->getQuery();
+        $query->instanceOf('');
+        $keywords  = $this->q;        
+        if ( strpos($keywords,' OR ') ) {
+            $keywords  = explode(' OR ',$keywords);
+            $operation = 'OR';
+        } else {
+            $keywords = explode(' ', $keywords);
+            $operation = 'AND';   
+        }
+        foreach($keywords as $keyword) {
+            $query->where('CONCAT(name,body) LIKE "%'.$keyword.'%"',$operation);
+        }
+        print_query($query);
+        die;
+        $entities = $query->fetchSet();
+        $this->_state->setList($entities);        
+        return $this->getView()->display();        
+    }
+}
