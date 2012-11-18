211da599c04c3f8f04e9731fb496205d2b1f3845
rmdstudio <rmdstudio@a933e228-e664-4d3f-8241-49479697c698>
2012-11-18 02:41:26 +0000

    using pagination rather than scroll for the search results
    git-svn-id: file:///var/www/svn/anahita/anahita/branches/search@16148 a933e228-e664-4d3f-8241-49479697c698

--- code/site/components/com_search/views/searches/html/list.php
+++ code/site/components/com_search/views/searches/html/list.php
@@ -7,12 +7,8 @@
 	<?php endforeach; ?>
 </div>
 
-<?php if ( !isset($pagination) || $pagination !== false ) : ?>
-<div id="an-more-records" class="an-more-records">
-<?= empty($pagination) ? @pagination($items, array('url'=>@route('layout=list&q='.$q))) : $pagination?>
-</div>
-<?php endif;?>
-
 <?php else : ?>
 <?= @message(@text('LIB-AN-PROMPT-NO-MORE-RECORDS-AVAILABLE')) ?>
 <?php endif; ?>
+
+<?= @pagination($items, array('url'=>@route('layout=list&q='.$q),'paginate'=>true)) ?>
