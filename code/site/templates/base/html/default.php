64fc90fc71edcc1528b6b8cf28ad38a1a85b84e4
asanieyan <asanieyan@a933e228-e664-4d3f-8241-49479697c698>
2013-01-17 23:03:53 +0000

    no fav icon in the template for now
    git-svn-id: file:///var/www/svn/anahita/anahita/branches/search@16500 a933e228-e664-4d3f-8241-49479697c698

--- code/site/templates/base/html/default.php
+++ code/site/templates/base/html/default.php
@@ -3,7 +3,7 @@
 <html lang="en">
   <head>
   	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?= @render('favicon') ?>
+    <link rel="shortcut icon" href="base://images/''" />
   	<?= @render('style') ?>
   </head>
 
