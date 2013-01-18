1ff866f71257be389356b73adc243c308056eb92
rmdstudio <rmdstudio@a933e228-e664-4d3f-8241-49479697c698>
2013-01-18 01:22:34 +0000

    hiding the prompt on the initial page
    git-svn-id: file:///var/www/svn/anahita/anahita/branches/search@16520 a933e228-e664-4d3f-8241-49479697c698

--- code/site/components/com_search/views/searches/html/list.php
+++ code/site/components/com_search/views/searches/html/list.php
@@ -1,12 +1,14 @@
 <?php defined('KOOWA') or die; ?>
 
 <div class="an-entities" id="an-entities-main">
-<?php if(isset($keywords) && count($items)) :?>
-	<?php foreach($items as $item ) : ?>
-		<?= @view('search')->layout('list')->item($item)->keywords($keywords)?>
-	<?php endforeach; ?>
-<?php else : ?>
-	<?= @message(@text('LIB-AN-PROMPT-NO-MORE-RECORDS-AVAILABLE')) ?>
+<?php if(isset($keywords)): ?>
+	<?php if(count($items)) :?>
+		<?php foreach($items as $item ) : ?>
+			<?= @view('search')->layout('list')->item($item)->keywords($keywords)?>
+		<?php endforeach; ?>
+	<?php else : ?>
+		<?= @message(@text('LIB-AN-PROMPT-NO-MORE-RECORDS-AVAILABLE')) ?>
+	<?php endif; ?>
 <?php endif; ?>
 </div>
 
