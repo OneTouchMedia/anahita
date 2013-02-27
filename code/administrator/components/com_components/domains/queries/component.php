b8ab4088e16276b23e4246bfd736ed2eb4878b3e
asanieyan <asanieyan@a933e228-e664-4d3f-8241-49479697c698>
2013-02-27 07:28:18 +0000

    only show the assignable components
    git-svn-id: file:///var/www/svn/anahita/anahita/branches/search@16661 a933e228-e664-4d3f-8241-49479697c698

---
+++ code/administrator/components/com_components/domains/queries/component.php
@@ -0,0 +1,40 @@
+<?php
+
+/** 
+ * LICENSE: ##LICENSE##
+ * 
+ * @category   Anahita
+ * @package    Com_Components
+ * @subpackage Domain_Query
+ * @author     Arash Sanieyan <ash@anahitapolis.com>
+ * @author     Rastin Mehr <rastin@anahitapolis.com>
+ * @copyright  2008 - 2010 rmdStudio Inc./Peerglobe Technology Inc
+ * @license    GNU GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
+ * @version    SVN: $Id$
+ * @link       http://www.anahitapolis.com
+ */
+
+/**
+ * Component query
+ *
+ * @category   Anahita
+ * @package    Com_Components
+ * @subpackage Domain_Query
+ * @author     Arash Sanieyan <ash@anahitapolis.com>
+ * @author     Rastin Mehr <rastin@anahitapolis.com>
+ * @license    GNU GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
+ * @link       http://www.anahitapolis.com
+ */
+class ComComponentsDomainQueryComponent extends LibComponentsDomainQueryComponent
+{
+	/**
+	 * Provides option to return assignable/nonassigable components
+	 * 
+	 * @return void
+	 */
+	protected function _beforeQueryBuild()
+	{
+		parent::_beforeQueryBuild();
+		$this->option($this->getService('com://admin/components.domain.set.assignablecomponent')->option);
+	}
+}
