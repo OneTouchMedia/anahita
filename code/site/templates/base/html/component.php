bbc976e20b5919d0426931c41cab9a6c5ef96451
asanieyan <asanieyan@a933e228-e664-4d3f-8241-49479697c698>
2013-04-03 18:31:53 +0000

    remove favicon for now
    git-svn-id: file:///var/www/svn/anahita/anahita/branches/search@16837 a933e228-e664-4d3f-8241-49479697c698

--- code/site/templates/base/html/component.php
+++ code/site/templates/base/html/component.php
@@ -2,8 +2,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<?= @render('favicon') ?>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">		
 		<?= @render('style') ?>        
     </head>
     <body id="tmpl-component">        
