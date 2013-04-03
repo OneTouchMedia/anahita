56590bfbce129fa1b58b3855b7e6971be40481f1
asanieyan <asanieyan@a933e228-e664-4d3f-8241-49479697c698>
2013-04-03 18:26:14 +0000

    a behavior for the controller to store messages in the flash. this is a temporary measure
    git-svn-id: file:///var/www/svn/anahita/anahita/branches/search@16827 a933e228-e664-4d3f-8241-49479697c698

--- code/site/components/com_base/controllers/resource.php
+++ code/site/components/com_base/controllers/resource.php
@@ -56,7 +56,8 @@ class ComBaseControllerResource extends LibBaseControllerResource
 	{
 		$config->append(array(
             'viewer'        => get_viewer(),
-		    'language'      => 'com_'.$this->getIdentifier()->package
+		    'language'      => 'com_'.$this->getIdentifier()->package,
+		    'behaviors'     => array('com://site/application.controller.behavior.message')
 		));
 				
 		parent::_initialize($config);
