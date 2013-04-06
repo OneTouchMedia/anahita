8c745f79f86db2bdd9dfd31708956e770460defc
asanieyan <asanieyan@a933e228-e664-4d3f-8241-49479697c698>
2013-04-06 01:47:02 +0000

    login modal records the current url as return
    git-svn-id: file:///var/www/svn/anahita/anahita/branches/search@16874 a933e228-e664-4d3f-8241-49479697c698

--- code/site/modules/mod_viewer/html/login.php
+++ code/site/modules/mod_viewer/html/login.php
@@ -1,6 +1,6 @@
 <ul class="menu nav" data-behavior="BS.Dropdown">
      <li class="dropdown">
-        <a data-trigger="BS.showPopup" data-bs-showpopup-url="<?= @route('option=people&view=session&layout=modal')?>" href="<?= @route('option=people&view=session&layout=modal')?>" class="dropdown-toggle">
+        <a data-trigger="BS.showPopup" data-bs-showpopup-url="<?= @route('option=people&view=session&layout=modal&return='.base64_encode(KRequest::url()))?>" href="<?= @route('option=people&view=session&layout=modal')?>" class="dropdown-toggle">
             Login                                               
         </a>
      </li>
