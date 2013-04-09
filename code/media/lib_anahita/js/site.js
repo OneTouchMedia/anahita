cecc1aa1526d0d00b315be2d7355631cd69eb193
asanieyan <asanieyan@a933e228-e664-4d3f-8241-49479697c698>
2013-04-09 22:56:10 +0000

    fixed issue with submitting form but pressing enter key
    git-svn-id: file:///var/www/svn/anahita/anahita/branches/search@16933 a933e228-e664-4d3f-8241-49479697c698

--- code/media/lib_anahita/js/site.js
+++ code/media/lib_anahita/js/site.js
@@ -544,37 +544,6 @@ Delegator.register('click', {
 		}
 		else submit();		
 	},
-	'Submit' : function(event, el, api) {
-		event.stop();
-		if ( el.hasClass('disabled') ) {
-		    return false;
-		}
-		if ( api.get('form') ) {
-			var form = document.getElement(api.get('form'));
-			var submit = function() {
-				form.spin();
-				form.submit();			
-			}
-		} else {
-			data = el.get('href').toURI().getData();
-			var form = Element.Form({action:el.get('href'), data:data});
-			if ( el.get('target') ) {
-				form.set('target', el.get('target'));
-			}	
-			var submit = function(){
-				el.spin();
-				form.inject(document.body, 'bottom');
-				form.submit();			
-			}			
-		}
-		
-		if ( api.get('promptMsg') ) {
-			api.get('promptMsg').prompt({onConfirm:submit});
-		}		
-		else {			
-			submit();
-		}
-	},
 	'VoteLink' : function(event, el, api) {
 		event.stop();
 		el.ajaxRequest({
@@ -597,6 +566,67 @@ Delegator.register('click', {
 	}
 });
 
+(function() {
+	Behavior.addGlobalFilter('Submit', {
+    	setup : function(el, api)
+    	{
+    		var form = document.getElement(api.get('form') || el);
+    		
+    		if ( !form ) 
+    			return;
+			
+    		var submit = function() {
+				form.spin();
+				form.submit();			
+			}
+    		if ( el != form ) {
+    			el.addEvent('click', submit);
+    		}
+    		form.addEvent('keyup', function(e) {
+    			if ( e.key == 'enter' ) {
+    				submit();
+    			}
+    		});
+    	}
+	});	
+	Delegator.register('click', {
+		'Submit' : function(event, el, api) {
+			event.stop();
+			if ( el.hasClass('disabled') ) {
+			    return false;
+			}
+			if ( api.get('form') ) 
+			{
+				var form = document.getElement(api.get('form'));
+				var submit = function() {
+					form.spin();
+					form.submit();			
+				}
+			} else {
+				var url    = api.get('url') || el.get('href');
+				var data   = JSON.encode(api.get('data')) || el.get('href').toURI().getData();
+				var target = api.get('target') || el.get('target');
+				var form   = Element.Form({action:url, data:data});
+				var spinner   = el.getElement(api.get('spinner')) || el;
+				if ( target ) {
+					form.set('target', target);
+				}	
+				var submit = function(){
+					if ( spinner ) spinner.spin();
+					form.inject(document.body, 'bottom');
+					form.submit();			
+				}
+			}
+			
+			if ( api.get('promptMsg') ) {
+				api.get('promptMsg').prompt({onConfirm:submit});
+			}		
+			else {			
+				submit();
+			}
+		}		
+	});
+})();
 (function() {
 	var remote_popups = {};
 	Delegator.register('click', 'BS.showPopup', {
