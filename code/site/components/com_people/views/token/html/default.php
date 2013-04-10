62acb880574701f6abf7b8e583d399fd86a0fad0
asanieyan <asanieyan@a933e228-e664-4d3f-8241-49479697c698>
2013-04-10 06:32:28 +0000

    display token error messages
    git-svn-id: file:///var/www/svn/anahita/anahita/branches/search@16949 a933e228-e664-4d3f-8241-49479697c698

--- code/site/components/com_people/views/token/html/default.php
+++ code/site/components/com_people/views/token/html/default.php
@@ -1,10 +1,19 @@
 <?php defined('KOOWA') or die('Restricted access') ?>
+<?php 
+@service('application.dispatcher')->tmpl = 'component';
+?>
 
 <div class="row">
 	<div class="offset3 span6">
 		<form data-behavior="FormValidator" action="<?= @route('view=token') ?>" method="post" class="well">
 		<fieldset>
 			<legend><?= @text('COM-PEOPLE-TOKEN-FORGOT-PASSWORD') ?></legend>
+            <?php if ( !empty($flash['confirm']) ) : ?>
+                 <?= @message(@text('COM-PEOPLE-TOKEN-SENT'), array('type'=>'info')) ?>            
+            <?php elseif ( !empty($flash['error']) ) : ?>
+            <?= @message(@text('COM-PEOPLE-TOKEN-INVALID-EMAIL'), array('type'=>'error')) ?> 
+            <?php endif?>			
+            
 			<div class="control-group">				
 				<div class="controls">
 					<p><?= @text('COM-PEOPLE-TOKEN-FORGOT-PASSWORD-INSTRUCTIONS') ?></p>
--- code/site/components/com_people/views/token/html/email_sent.php
+++
@@ -1,5 +0,0 @@
-asdfasdf
-sad
-fas
-df
-asdf
