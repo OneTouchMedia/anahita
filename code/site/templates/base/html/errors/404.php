c0e831c13a1445e9bcd4cffbf3f434c704527b64
asanieyan <asanieyan@a933e228-e664-4d3f-8241-49479697c698>
2013-04-13 21:03:19 +0000

    testing svn scrpit
    git-svn-id: file:///var/www/svn/anahita/anahita/branches/search@16967 a933e228-e664-4d3f-8241-49479697c698

--- code/site/templates/base/html/errors/404.php
+++ code/site/templates/base/html/errors/404.php
@@ -6,5 +6,5 @@
 
 <div class="alert alert-block alert-error">
 <h4 class="alert-heading"><?php print JText::_('TMPL-ERROR-404-TITLE') ?></h4>
-<p><?php print JText::_('TMPL-ERROR-404-DESC') ?></p>
+<p><?php print JText::_('TMPL-ERROR-404-DESC') ?></p> 
 </div>
