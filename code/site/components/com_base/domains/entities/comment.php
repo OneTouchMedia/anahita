09eb90c8eb1ccfd7ebe1ae64892ea19f2549fa9d
asanieyan <asanieyan@a933e228-e664-4d3f-8241-49479697c698>
2013-04-15 22:04:24 +0000

    the default comment parent is node
    git-svn-id: file:///var/www/svn/anahita/anahita/branches/search@17000 a933e228-e664-4d3f-8241-49479697c698

--- code/site/components/com_base/domains/entities/comment.php
+++ code/site/components/com_base/domains/entities/comment.php
@@ -43,16 +43,16 @@ class ComBaseDomainEntityComment extends ComBaseDomainEntityNode
 			'attributes' => array(
 				'body'			=> array('required'=>true)
 			),
-			'behaviors'		=> array(                
-				'parentable' => array('parent'=>'node'),
-                'taggable',
+			'behaviors'		=> array(       
+			    'parentable' => array('parent'=>'com:base.domain.entity.node'),			                 				
+                //'taggable',
 				'modifiable',
 				'authorizer',
 				'locatable' ,
 				'votable'
 			)
-		));
-					
+		));					
+		
 		parent::_initialize($config);
 	}
 	
