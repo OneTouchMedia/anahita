a5b126f75827a5c116a174eb6e04c3e7e21d2572
rmdstudio <rmdstudio@a933e228-e664-4d3f-8241-49479697c698>
2013-04-19 21:57:34 +0000

    global meta description and keywords have been removed from the global configuration forms to prevent confusion.
    git-svn-id: file:///var/www/svn/anahita/anahita/branches/search@17038 a933e228-e664-4d3f-8241-49479697c698

--- code/site/components/com_search/views/searches/html/default.php
+++ code/site/components/com_search/views/searches/html/default.php
@@ -12,7 +12,7 @@
 
 
 <?php if(empty($keywords)) : ?>
-<form action="<?=@route('view=searches')?>">
+<form action="<?=@route('view=searches')?>" class="well">
 	<fieldset>
 		<legend><?= @text('COM-SEARCH-PROMPT') ?></legend>
 		<input type="text" name="q" class="input-block-level">
