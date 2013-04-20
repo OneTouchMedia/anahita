dafe223a1e7e01cb1ec492046012e6c5aa070d9c
rmdstudio <rmdstudio@a933e228-e664-4d3f-8241-49479697c698>
2013-04-20 06:02:11 +0000

    Made the node read view more like a standard medium
    git-svn-id: file:///var/www/svn/anahita/anahita/branches/search@17055 a933e228-e664-4d3f-8241-49479697c698

--- code/site/components/com_notes/views/note/html/default.php
+++ code/site/components/com_notes/views/note/html/default.php
@@ -4,16 +4,6 @@
 
 <module position="sidebar-b" style="simple"></module>
 
-<div class="an-story an-entity an-record an-removable">
-    <div class="story-avatar">
-        <?= @avatar($note->author) ?>
-    </div>      
-    <div class="story-container">
-        <div class="story-body">
-            <?= @content($note->body) ?>
-        </div>
-        <div class="story-comments">
-            <?= @helper('ui.comments', $note) ?>  
-        </div> 
-    </div>
-</div>
+<?= @template('note') ?>
+
+<?= @helper('ui.comments', $note) ?>
