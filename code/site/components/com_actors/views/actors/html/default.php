fdddd6415d96661fb9e239a0b61e7472e374ef50
rmdstudio <rmdstudio@a933e228-e664-4d3f-8241-49479697c698>
2013-04-20 04:48:13 +0000

    removed the filter box
    git-svn-id: file:///var/www/svn/anahita/anahita/branches/search@17053 a933e228-e664-4d3f-8241-49479697c698

--- code/site/components/com_actors/views/actors/html/default.php
+++ code/site/components/com_actors/views/actors/html/default.php
@@ -2,8 +2,6 @@
 
 <module position="sidebar-b" style="simple"></module>
 
-<?= @helper('ui.filterbox', @route('layout=list'))?>
-
 <div class="an-entities-wrapper">	
 	<?= @template('list') ?>
 </div>
