62fa2c939f9c5d0ff01248e7384306a07796699e
rmdstudio <rmdstudio@a933e228-e664-4d3f-8241-49479697c698>
2013-04-20 04:32:01 +0000

    simplified search options
    git-svn-id: file:///var/www/svn/anahita/anahita/branches/search@17051 a933e228-e664-4d3f-8241-49479697c698

--- code/site/components/com_search/views/searches/html/list.php
+++ code/site/components/com_search/views/searches/html/list.php
@@ -1,30 +1,28 @@
 <?php defined('KOOWA') or die; ?>
 
-<div class="an-meta"><?= sprintf(@text('COM-SEARCH-RESULTS-FOUND'), $items->getTotal()) ?></div>
+<div class="an-meta">
+	<p><?= sprintf(@text('COM-SEARCH-RESULTS-FOUND'), $items->getTotal()) ?></p>
+</div>
 
 <?php if ( $current_scope && ($current_scope->commentable || $current_scope->ownable) && $items->getTotal() ) : ?>
 <form>
-	<fieldset>
-		<legend><?= @text('COM-SEARCH-OPTIONS') ?></legend>
-		<?php if ( $current_scope->ownable ) : ?>
-		<label><?= @text('COM-SEARCH-OPTION-SORT') ?></label>
-		<select data-trigger="SortOption" name="sort">
-			<option <?= ($sort == 'relevant') ? 'selected' : '' ?> value="relevant">
-				<?= @text('COM-SEARCH-OPTION-SORT-MOST-RELEVANT') ?>
-			</option>
-			<option <?= ($sort == 'recent') ? 'selected' : '' ?> value="recent">
-				<?= @text('COM-SEARCH-OPTION-SORT-MOST-RECENT') ?>
-			</option>
-		</select>
-		<?php endif;?>
-		
-		<?php if ($current_scope->commentable) : ?>
-		<label class="checkbox">
-			<input data-trigger="SearchOption" <?= $search_comments ? 'checked' : ''?> type="checkbox" name="search_comments" value="1" >
-			<?= @text('COM-SEARCH-OPTION-COMMENTS') ?>
-	    </label>
-	    <?php endif;?>
-	</fieldset>
+	<?php if ( $current_scope->ownable ) : ?>
+	<select data-trigger="SortOption" name="sort">
+		<option <?= ($sort == 'relevant') ? 'selected' : '' ?> value="relevant">
+			<?= @text('COM-SEARCH-OPTION-SORT-MOST-RELEVANT') ?>
+		</option>
+		<option <?= ($sort == 'recent') ? 'selected' : '' ?> value="recent">
+			<?= @text('COM-SEARCH-OPTION-SORT-MOST-RECENT') ?>
+		</option>
+	</select>
+	<?php endif;?>
+	
+	<?php if ($current_scope->commentable) : ?>
+	<label class="checkbox">
+		<input data-trigger="SearchOption" <?= $search_comments ? 'checked' : ''?> type="checkbox" name="search_comments" value="1" >
+		<?= @text('COM-SEARCH-OPTION-COMMENTS') ?>
+    </label>
+    <?php endif;?>
 </form>
 <?php endif;?>
 
