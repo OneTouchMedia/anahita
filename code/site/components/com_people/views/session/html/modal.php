67d0eae6b2ba716f061a8c5d4d320789979c5630
asanieyan <asanieyan@a933e228-e664-4d3f-8241-49479697c698>
2013-04-24 00:55:09 +0000

    cleaned up session modal
    git-svn-id: file:///var/www/svn/anahita/anahita/branches/search@17111 a933e228-e664-4d3f-8241-49479697c698

--- code/site/components/com_people/views/session/html/modal.php
+++ code/site/components/com_people/views/session/html/modal.php
@@ -1,53 +1,53 @@
 <?php defined('KOOWA') or die('Restricted access') ?>
-<popup>    
-    <popup:header>
-    	<a href="#" class="close">x</a>
-    	<h3><?= @text('COM-PEOPLE-SESSION-TITLE') ?></h3>
-    </popup:header>
     
-    <popup:body>
-    	<form id="modal-login-form" action="<?=@route()?>" method="post">
-			<?php KService::get('koowa:loader')->loadIdentifier('com://site/connect.template.helper.service')?>
-			<?php if ( class_exists('ComConnectTemplateHelperService', true) ): ?>
-			<p><?= @text('COM-PEOPLE-SESSION-CONNECT-PROMPT-DESCRIPTION') ?></p>
-			<div class="connect-service-actions">
-				<?= KService::get('com://site/connect.template.helper.service')->renderLogins() ?>
-			</div>
-			<?php endif; ?>
-			
-			<div class="control-group">			
-				<div class="controls">
-					<input class="input-block-level" name="username" placeholder="<?= @text('COM-PEOPLE-SESSION-PLACEHOLDER-USERNAME-EMAIL')?>" id="username" type="text" alt="username" size="18" />
-				</div>
-			</div>
-			
-			<div class="control-group">				
-				<div class="controls">
-					<input class="input-block-level" type="password" placeholder="<?= @text('COM-PEOPLE-SESSION-PLACEHOLDER-PASSWORD')?>" id="passwd" name="password" size="18" alt="password" />
-				</div>
-			</div>
-			
-			 
-			<div id="form-login-remember" class="control-group">
-				<label class="checkbox">
-					<input type="checkbox" name="remember" value="yes" alt="<?= @text('COM-PEOPLE-SESSION-REMEMBER-ME'); ?>" />
-					<?= @text('COM-PEOPLE-SESSION-REMEMBER-ME'); ?>
-				</label>
-			</div>
+<popup:header>
+	<a href="#" class="close">x</a>
+	<h3><?= @text('COM-PEOPLE-SESSION-TITLE') ?></h3>
+</popup:header>
+
+<div id="flash-message"></div>
+<form id="modal-login-form" action="<?=@route()?>" method="post">
+	<?php KService::get('koowa:loader')->loadIdentifier('com://site/connect.template.helper.service')?>
+	<?php if ( class_exists('ComConnectTemplateHelperService', true) ): ?>
+	<p><?= @text('COM-PEOPLE-SESSION-CONNECT-PROMPT-DESCRIPTION') ?></p>
+	<div class="connect-service-actions">
+		<?= KService::get('com://site/connect.template.helper.service')->renderLogins() ?>
+	</div>
+	<?php endif; ?>
 	
-			<a href="<?= @route('view=token') ?>">
-			<?= @text('COM-PEOPLE-SESSION-FORGOT-PASSWORD'); ?>
-			</a>
-			
-    		<?php if ( !empty($return) ) : ?>
-    			<input type="hidden" name="return" value="<?= $return; ?>" />
-    		<?php endif;?>
-		</form>
-    </popup:body>
-    
-    <popup:footer>
-        <button data-behavior="Submit" data-submit-form="#modal-login-form" name="Submit" class="btn btn-large btn-primary">
-        	<?= @text('COM-PEOPLE-ACTION-LOGIN') ?>
-        </button>    
-    </popup:footer>
-</popup>
+	<div class="control-group">			
+		<div class="controls">
+			<input class="input-block-level" name="username" placeholder="<?= @text('COM-PEOPLE-SESSION-PLACEHOLDER-USERNAME-EMAIL')?>" id="username" type="text" alt="username" size="18" />
+		</div>
+	</div>
+	
+	<div class="control-group">				
+		<div class="controls">
+			<input class="input-block-level" type="password" placeholder="<?= @text('COM-PEOPLE-SESSION-PLACEHOLDER-PASSWORD')?>" id="passwd" name="password" size="18" alt="password" />
+		</div>
+	</div>
+	
+	 
+	<div id="form-login-remember" class="control-group">
+		<label class="checkbox">
+			<input type="checkbox" name="remember" value="yes" alt="<?= @text('COM-PEOPLE-SESSION-REMEMBER-ME'); ?>" />
+			<?= @text('COM-PEOPLE-SESSION-REMEMBER-ME'); ?>
+		</label>
+	</div>
+
+	<a href="<?= @route('view=token') ?>">
+	<?= @text('COM-PEOPLE-SESSION-FORGOT-PASSWORD'); ?>
+	</a>
+	<?php if ( !empty($return) ) : ?>
+		<input type="hidden" name="return" value="<?= $return; ?>" />
+	<?php endif;?>
+</form>
+
+<popup:footer>
+     <a href="" data-trigger="BS.showPopup" data-bs-showpopup-url="<?=@route('option=com_people&view=person&layout=add&modal=1&return='.@$return)?>" class="pull-left">
+         <?= @text('COM-PEOPLE-ACTION-SIGNUP-NEW-ACCOUNT')?>
+     </a>
+    <button data-behavior="Request" data-request-form="#modal-login-form" name="Submit" class="btn btn-large btn-primary">
+    	<?= @text('COM-PEOPLE-ACTION-LOGIN') ?>
+    </button>    
+</popup:footer>
