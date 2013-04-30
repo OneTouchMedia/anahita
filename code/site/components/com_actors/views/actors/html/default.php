2ef6942182ce5bcc1a99c2568b54a46c6febf3f9
rmdstudio <rmdstudio@a933e228-e664-4d3f-8241-49479697c698>
2013-04-30 22:59:33 +0000

    added filterbox for the actors
    git-svn-id: file:///var/www/svn/anahita/anahita/branches/search@17168 a933e228-e664-4d3f-8241-49479697c698

--- code/site/components/com_actors/views/actors/html/default.php
+++ code/site/components/com_actors/views/actors/html/default.php
@@ -2,6 +2,8 @@
 
 <module position="sidebar-b" style="simple"></module>
 
+<?= @helper('ui.filterbox', @route('layout=list')) ?>
+
 <div class="an-entities-wrapper">	
 	<?= @template('list') ?>
 </div>
