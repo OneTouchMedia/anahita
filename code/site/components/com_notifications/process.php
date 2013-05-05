47e493128c5b17f71bc004a00712a2ea2f057fef
asanieyan <asanieyan@a933e228-e664-4d3f-8241-49479697c698>
2013-05-05 06:18:48 +0000

    fixed typo
    git-svn-id: file:///var/www/svn/anahita/anahita/branches/search@17230 a933e228-e664-4d3f-8241-49479697c698

--- code/site/components/com_notifications/process.php
+++ code/site/components/com_notifications/process.php
@@ -3,7 +3,7 @@ if ( !defined('JPATH_BASE') )
 {
     $base = dirname($_SERVER['DOCUMENT_ROOT'].$_SERVER['SCRIPT_NAME']);
     $base = str_replace('/components/com_notifications','',$base);
-    define(JPATH_BASE, $base);
+    define('JPATH_BASE', $base);
     require_once JPATH_BASE.'/includes/framework.php';
     KService::get('com://site/application.dispatcher')->load(); 
 }
